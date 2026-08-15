3aca38939163a11b3e2ae5506c23d6fd8c14b31a
a100447739 <callofairou@gmail.com>
2026-08-16 07:53:30 +0800

    savePoint

---
+++ class_機電物件.py
@@ -0,0 +1,13 @@
+class object:
+    def __init__(self, 項次:str, 項目及說明:str, 單位:str,數量,單價,複價,編碼_備註:str):
+        self.項次 = 項次
+        self.項目及說明 = 項目及說明
+        self.單位 = 單位
+        self.數量 = 數量
+        self.單價 = 單價
+        self.複價 = 複價
+        self.編碼備註 = 編碼_備註
+        
+    '''def 項次(self):
+        return self.項次'''
+        
